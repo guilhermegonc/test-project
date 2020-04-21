138b3faa83e106da7a25f36ef9e38964c742ac46
guilhermegonc <gonc.guilherme@mesalva.com>
2020-04-20 22:49:53 -0300

    remove-unused-code

--- app/views.py
+++ app/views.py
@@ -3,21 +3,11 @@ import requests
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Greeting
-
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
-def db(request):
-
-    greeting = Greeting()
-    greeting.save()
-
-    greetings = Greeting.objects.all()
-
-    return render(request, "db.html", {"greetings": greetings})
 
 def cta(request):
     return render(request, 'cta-page.html')

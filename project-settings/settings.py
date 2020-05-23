7ad40406e938feff1dd0f8290bcf32a32b1a049c
guilhermegonc <gonc.guilherme@mesalva.com>
2020-05-23 13:22:10 -0300

    Update settings.py

--- project-settings/settings.py
+++ project-settings/settings.py
@@ -11,14 +11,17 @@ DEBUG = True
 ALLOWED_HOSTS = ['http://guilhermegoncalves.herokuapp.com']
 
 
-if DEBUG is True:
-    with open('secret.txt', 'r') as file:
-        SECRET_KEY = file.read()
-    with open('db_secret.txt', 'r') as file:
-        PASSWORD = file.read()
-else: 
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    PASSWORD = os.environ.get('DB_PASSWORD')
+# if DEBUG is True:
+#     with open('secret.txt', 'r') as file:
+#         SECRET_KEY = file.read()
+#     with open('db_secret.txt', 'r') as file:
+#         PASSWORD = file.read()
+# else: 
+#     SECRET_KEY = os.environ.get('SECRET_KEY')
+#     PASSWORD = os.environ.get('DB_PASSWORD')
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+PASSWORD = os.environ.get('DB_PASSWORD')
 
 
 # Application definition

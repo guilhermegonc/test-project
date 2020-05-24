254a131bcc3ac2addf1cd02b91466aa2d4e3e1e4
guilhermegonc <gonc.guilherme@mesalva.com>
2020-05-24 11:05:16 -0300

    update mig

--- app/migrations/0001_initial.py
+++ app/migrations/0001_initial.py
@@ -13,11 +13,11 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.CreateModel(
-            name='Greeting',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('when', models.DateTimeField(auto_now_add=True, verbose_name=b'date created')),
-            ],
-        ),
+        # migrations.CreateModel(
+        #     name='Greeting',
+        #     fields=[
+        #         ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+        #         ('when', models.DateTimeField(auto_now_add=True, verbose_name=b'date created')),
+        #     ],
+        # ),
     ]

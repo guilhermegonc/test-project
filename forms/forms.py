fd1be1d5276fe74023ba21754044dbdff2cd587b
guilhermegonc <gonc.guilherme@mesalva.com>
2020-10-22 22:24:17 -0300

    Update forms.py

--- forms/forms.py
+++ forms/forms.py
@@ -3,3 +3,4 @@ from django import forms
 class BasicForm(forms.Form):
     name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Seu nome aqui'}))
     email = forms.EmailField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'email@mail.com'}))
+    random_big_number = forms.IntegerField(widget=forms.TextInput(attrs={'placeholder': '1111 1111 1111 1111'}))

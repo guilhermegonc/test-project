be8a98fd5bbfa7e60f0eee8049b7e0cb17a8ec0e
guilhermegonc <gonc.guilherme@mesalva.com>
2020-11-08 19:31:27 -0300

    update

--- smart_home/static/relay-control.js
+++ smart_home/static/relay-control.js
@@ -1,14 +1,33 @@
-let lampOn = true;
+const token = '1wSjC7iMMcKzvku4g-mGpOLPQzpQgLu0'
+const pin = 'D2'
+let lampOn = getLampStatus();
+
+async function getLampStatus() {
+    let url = `http://blynk-cloud.com/${token}/get/${pin}`
+    let status = await fetch(url)
+    status = await status.json() == "1"
+    changeText()
+}
 
 const setRelay = () => {
     let lampObj = document.querySelector('#lamp')
-    lampObj.onclick = () => changeText(lampObj)
+    lampObj.onclick = () => changeStatus()
 }
 
-const changeText = (appliance) => {
-    newVal = lampOn === true ? '💡' : '🔌'
-    appliance.innerText = newVal
-    changeStatus()
+const changeStatus = () => {
+    lampOn = !lampOn
+    changePin(lampOn)
 }
 
-const changeStatus = () => lampOn = !lampOn
+const changePin = (status) => {
+    let newVal = status == true ? '1' : '0'
+    let url = `http://blynk-cloud.com/${token}/update/${pin}?value=${newVal}`
+    fetch(url)
+    changeText()
+}
+
+const changeText = () => {
+    let lampObj = document.querySelector('#lamp')
+    newVal = lampOn == true ? '💡' : '🔌'    
+    lampObj.innerText = newVal
+}

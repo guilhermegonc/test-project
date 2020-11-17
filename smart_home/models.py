a70ae75d6a2e0ed36cfe796b5551136b0d40418a
guilhermegonc <gonc.guilherme@mesalva.com>
2020-11-16 23:29:44 -0300

    Update models.py

--- smart_home/models.py
+++ smart_home/models.py
@@ -1,8 +1,40 @@
 from django.db import models
 
-# # Create your models here.
-# class interactionEvents(models.Model):
-#     name = models.CharField(max_length=100)
-#     room_id = models.IntegerField()
-#     created_at = Datefield.auto_now_add
-#     updated_at = 
+
+class Accounts(models.Model):
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+class Users(models.Model):
+    auth0_id = models.CharField(max_length=255)
+    role = models.CharField(max_length=31)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+class Account_Users(models.Model):
+    account_id = models.ForeignKey('Accounts', on_delete=models.CASCADE)
+    user_id = models.ForeignKey('Users', on_delete=models.CASCADE)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+class Microcontrollers(models.Model):
+    token = models.CharField(max_length=255)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+class Microcontrollers_Accounts(models.Model):
+    microcontrollers_id = models.ForeignKey('Microcontrolllers', on_delete=models.CASCADE)
+    accounts_id = models.ForeignKey('Accounts', on_delete=models.CASCADE)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+class Devices(models.Model):
+    pin = models.CharField(max_length=31)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+class Microcontroller_Devices(models.Model):
+    microcontrollers_id = models.ForeignKey('Microcontrolllers', on_delete=models.CASCADE)
+    device_id = models.ForeignKey('Devices', on_delete=models.CASCADE)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)

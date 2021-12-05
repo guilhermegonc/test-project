e2e8dfbf1d1487095c7940e8030d17d671ea7b0b
Guilherme Gonçalves <gonc.guilherme@gmail.com>
2021-12-05 18:34:38 -0300

    Update log_payments.py

--- stocks_background/management/commands/log_payments.py
+++ stocks_background/management/commands/log_payments.py
@@ -6,7 +6,10 @@ from django.core.management.base import BaseCommand, CommandError
 from expenses_management.expensesHelper import create_expense
 from expenses_management.recurringHelper import get_recurring
 from app.userHelper import get_all_users
+from app.models import Users
 
+
+import sys
 
 class Command(BaseCommand):
     help = 'Update Recommendations'
@@ -20,13 +23,14 @@ class Command(BaseCommand):
         date = datetime.today().strftime('%Y-%m-01')
         users = get_all_users()
         to_register = [get_recurring(u.id, [True]) for u in users]
-        return [self.register_payment(ur, date) for ur in to_register]
+        return [self.register_payment(ur['data'], date) for ur in to_register]
 
 
     def register_payment(self, recurring, date):
         return [self.write_payload(r, date) for r in recurring]
 
+
     def write_payload(self, recurring, date):
-        payload = {'user': recurring.user, 'name': recurring.name, 'type': recurring.type, 
-            'date': date, 'value': recurring.value,'recurring': True}        
+        payload = {'user': Users.objects.get(id=recurring['user_id']), 'name': recurring['name'], 'type': recurring['type'], 
+            'date': date, 'value': recurring['value'], 'recurring': True}        
         return create_expense(payload)
